371ddf4ec6562dbed578216864b961a3611686f7
ViniDalvino <37189791+ViniDalvino@users.noreply.github.com>
2020-01-21 22:12:04 -0500

    Specified on what return the function

--- src/ReplicatedStorage/DominoPizzaAPI/StoreLocator/GetStoreID/GetStoreID.lua
+++ src/ReplicatedStorage/DominoPizzaAPI/StoreLocator/GetStoreID/GetStoreID.lua
@@ -2,7 +2,7 @@ local GetStoreId = {}
 
 local HttpService = game:GetService("HttpService")
 
-function GetStoreId.GetStoreId() -- return an array of all the store ids
+function GetStoreId.GetStoreId() -- return an array of all the store id's (key : store location, value : store ID) 
     NearByStore = require(game.ReplicatedStorage.DominoPizza)
     
 end
