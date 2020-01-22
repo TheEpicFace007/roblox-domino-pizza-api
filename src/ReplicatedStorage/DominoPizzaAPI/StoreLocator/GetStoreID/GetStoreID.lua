96c441b65d4add6d17a526aaf90c03a15b939d64
ViniDalvino <37189791+ViniDalvino@users.noreply.github.com>
2020-01-21 20:19:42 -0500

    Separated the module into different modules script
    ###### Why did I decided to split the module into diferent module and folder :
    It's easier to manage diferent file while we have error
    
    - Created an URL module to easily modify the url 
    - Created an StoreLocator and folder to separate the locating of stores into a dedicated folder
    
    I will soon switch to git instead of manually putting the file through github.

---
+++ src/ReplicatedStorage/DominoPizzaAPI/StoreLocator/GetStoreID/GetStoreID.lua
@@ -0,0 +1,10 @@
+local GetStoreId = {}
+
+local HttpService = game:GetService("HttpService")
+
+function GetStoreId.GetStoreId() -- return an array of all the store ids
+    NearByStore = require(game.ReplicatedStorage.DominoPizza)
+    
+end
+
+return GetStoreId
---
+++ src/ReplicatedStorage/DominoPizzaAPI/URL.lua
@@ -0,0 +1,12 @@
+local DominoUrl = {}
+
+DominoUrl.URL = {
+    Canada = {
+        StoreLocator = 'http://order.dominos.ca/power/store-locator?type=Locations&c='
+    },
+    UnitedStates = {
+        StoreLocator = ''
+    },
+}
+
+return DominoUrl
