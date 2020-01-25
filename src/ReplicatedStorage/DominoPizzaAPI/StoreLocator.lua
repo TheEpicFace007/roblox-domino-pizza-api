ac1ce0e3acdd50cfd5417cd18e54dd0a335e51f1
vinidalvino <vinidalvino@cock.li>
2020-01-25 14:31:29 -0500

    Captalized the variable to make it fancier

--- src/ReplicatedStorage/DominoPizzaAPI/StoreLocator.lua
+++ src/ReplicatedStorage/DominoPizzaAPI/StoreLocator.lua
@@ -3,15 +3,17 @@ local HttpService = game:GetService("HttpService")
 
 local StoreLocator = {}
 -- to do : use new luau variable type
-function StoreLocator.StoreSearch(city,province,zipcode) -- Get all nearby store info in a array <array> StoreSearch(<string> city, <string> province, <string> zipcode)
+function StoreLocator.StoreSearch(City,Province,Zipcode) -- Get all nearby store info in a array <array> StoreSearch(<string> City, <string> Province, <string> Zipcode)
     -- Error handling
-    assert(city,'Missing argument : City missing')
-    assert(province,'Missing argument : Province missing')
-    assert(zipcode,'Missing argument  : Zipcode missing')
+    assert(City,'Missing argument : City missing')
+    assert(Province,'Missing argument : Province missing')
+    assert(Zipcode,'Missing argument  : Zipcode missing')
     -- Script
-    local url = DominoURL.URL.Canada.StoreLocator .. city .. '%20' .. province .. '%20' .. zipcode
+    local url = DominoURL.URL.Canada.StoreLocator .. City .. '%20' .. Province .. '%20' .. Zipcode
     local Data = HttpService:JSONDecode((HttpService:GetAsync(url)))
-    if Data.status == -104 then error('You did not entered the store information correctly. Check if you entred the Address corretcly',2) end
+    if Data.status == -104 then 
+        error('You did not entered the store information correctly. Check if you entred the Address corretcly',2) 
+    end
     return Data
 end
 
