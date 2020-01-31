a08ca52f549cbd1b5b3e1c1da7fbefd41f639d2d
vinidalvino <vinidalvino@cock.li>
2020-01-31 14:10:56 -0500

    Added canada menu link

--- src/ReplicatedStorage/DominoPizzaAPI/URL.lua
+++ src/ReplicatedStorage/DominoPizzaAPI/URL.lua
@@ -2,7 +2,8 @@ local DominoUrl = {}
 
 DominoUrl.URL = {
     Canada = {
-        StoreLocator = 'http://order.dominos.ca/power/store-locator?type=Locations&c='
+        StoreLocator = 'http://order.dominos.ca/power/store-locator?type=Locations&c=',
+        Menu = 'http://order.dominos.ca/power/store/'-- 'STOREID/menu?lang=en&structured=true'
     },
     UnitedStates = {
         StoreLocator = ''
