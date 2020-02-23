cf7550c8bebcf71b9765a30732d3e3d535097b18
vinidalvino <vinidalvino@cock.li>
2020-02-23 18:34:20 -0500

    inserted order endpoint url

--- src/ReplicatedStorage/DominoPizzaAPI/URL.lua
+++ src/ReplicatedStorage/DominoPizzaAPI/URL.lua
@@ -3,7 +3,8 @@ local DominoUrl = {}
 DominoUrl.URL = {
     Canada = {
         StoreLocator = 'http://order.dominos.ca/power/store-locator?type=Locations&c=',
-        Menu = 'http://order.dominos.ca/power/store/'-- 'STOREID/menu?lang=en&structured=true'
+        Menu = 'http://order.dominos.ca/power/store/', -- 'STOREID/menu?lang=en&structured=true',
+        Order = 'https://order.dominos.ca/power/place-order'
     },
     UnitedStates = {
         StoreLocator = ''
